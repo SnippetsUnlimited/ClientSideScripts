545bcb8f15da798d00c93de0a1d586f67360ef9e
SnippetsUnlimited <ayyaz_ahmed@hotmail.com>
2014-02-11 07:12:47 -0500

    update

---
+++ Company.Web.JQuery.InputWizard/Scripts/inputWizard.js
@@ -0,0 +1,317 @@
+﻿(function ($) {
+
+    "use strict";
+
+    // global callback manager.
+    var searchOptimizer = function (func) {
+        var args, release, blocked;
+
+        var release = function () { blocked = false; };
+        if (blocked) {
+            console.log("blocked");
+            return;
+        }
+
+        blocked = true;
+        args = Array.prototype.slice.call(arguments);
+        args.unshift(release);
+        func.apply(this, args);
+    };
+
+    var isPrintable = function (code) {
+        return (
+            (code > 47 && code < 58) || // number keys
+            (code === 32) || // spacebar & return key(s) (if you want to allow carriage returns)
+            //(code === 13) ||
+            (code > 64 && code < 91) || // letter keys
+            (code > 95 && code < 112) || // numpad keys
+            (code > 185 && code < 193) || // ;=,-./` (in order)
+            (code > 218 && code < 223));   // [\]' (in order)
+    };
+
+    $.inputWizard = function (element, options) {
+
+        var $element = $(element);
+
+        var _Settings = $.extend({}, {
+            popupClass: "selectorlist",
+            popupItemClass: "selectorlist-item",
+            selectedpopupItemClass: "selectorlist-selecteditem",
+            inputManagerClass: "inputmanager-main",
+            fullTextMatching: true,
+            selectOnSpacebar: true,
+            triggers: null,
+            events: {
+                onDataLoad: null
+            },
+
+        }, options);
+
+        var searchCache = (function () {
+            var cache = {};
+
+            return {
+                setSearchCache: function (term, results) {
+                    cache[term] = results;
+                },
+                getSearchCache: function (term) {
+                    return cache[term];
+                }
+            }
+        })();
+
+        var _OptionSelector = new optionSelector({
+            listClass: _Settings.popupClass,
+            itemClass: _Settings.popupItemClass,
+            selectedItemClass: _Settings.selectedpopupItemClass,
+            events: {
+                onClicked: function (e) {
+                    $element.focus();
+                    this.setVisible(false);
+                    processSelection(this, $element);
+                }
+            }
+        });
+
+        // make option selector invisible initially.
+        _OptionSelector.setVisible(false);
+
+        // save types text to compare with new and detect the changes
+        var _CachedText = "";
+        var _CachedLocation = 0;
+
+        var _InputManager = new inputManager($element, {
+            containerClass: _Settings.inputManagerClass,
+            events: {
+                onKeyup: function (e) {
+                    var $selector = _OptionSelector;
+                    var preLength = e.preText.length;
+                    var keyCode = e.keyCode;
+                    var ctrlKey = e.ctrlKey;
+                    var fullTextMatching = _Settings.fullTextMatching;
+                    var forcePopup = false;
+                    var preText = e.preText;
+
+
+                    // if fullTextMatching is disabled then check for ctrl+space to activate the popup/optionselector
+                    if (!fullTextMatching && ctrlKey == true && keyCode == 32) {
+                        e.preventDefault();
+                        forcePopup = true;
+                    }
+
+                    // use option selector if text under the cursor is changed or ctrl+space is clicked 
+                    if (forcePopup || (preText && preLength > 0 && _CachedText != preText)) {
+                        _CachedText = preText;
+
+                        console.log(keyCode);
+
+                        // force popup anyways or else check if there was a ligitimate printable key was pressed.
+                        if (!forcePopup) {
+                            if (!$selector.getVisibility() && !(isPrintable(keyCode) || keyCode > 255)) {
+                                return;
+                            }
+                        }
+
+                        console.log(e.preText);
+
+
+                        // dataset represents a set trigger, term and results.
+                        var dataset = null;
+
+                        // check which trigger triggers search in order.
+                        $.each(_Settings.triggers, function (key, trigger) {
+                            var result = null;
+                            var matches = null;
+
+                            // if autopoup is enable and a force popup open is detected get matches. we will need this.
+                            if (fullTextMatching || forcePopup) {
+                                matches = preText.match(trigger.match);
+                            }
+
+                            // if autopop is is disabled we still need to load matches based on cursor location.
+                            if (!fullTextMatching) {
+
+                                // we need to know the cursor location.
+                                if (!$selector.getVisibility()) {
+
+                                    if (forcePopup && matches) {
+                                        // if forcepopup is detected use matches to get to get cursor location. 
+                                        _CachedLocation = preText.length - matches[matches.length - 1].length;
+                                    }
+                                    else {
+                                        // last entered character is upon the cursor location.
+                                        _CachedLocation = preText.length - 1;
+                                    }
+                                }
+                                // get matches again based on the text selected
+                                matches = preText.substring(_CachedLocation).match(trigger.match);
+                            }
+
+
+
+                            // if there are matches break the loop with findings.
+                            if (matches) {
+                                var term = matches[matches.length - 1];
+                                var cache = searchCache.getSearchCache(term);
+                                if (cache && cache[0] == trigger) {
+                                    result = cache[1];
+                                }
+                                else {
+                                    searchOptimizer(function (releaseCallback) {
+                                        result = trigger.search(term);
+                                        releaseCallback();
+                                    });
+                                }
+
+                                searchCache.setSearchCache(term, [trigger, result]);
+                                dataset = [trigger, term, result];
+                                return;
+                            }
+                        });
+
+                        // decide whether the popup should finally be displayed or not.
+                        var display = (function (ds) {
+                            var data = ds ? ds[2] : null;
+                            if (data && data.length > 0) {
+                                var showList = true;
+                                if (_Settings.events.onDataLoad) {
+                                    showList = _Settings.events.onDataLoad.call(this, data);
+                                }
+                                $selector.tag = { preText: e.preText, postText: e.postText, data: ds };
+                                $selector.setData(data);
+                                return showList;
+                            }
+                            $selector.tag = null;
+                            return false;
+                        })(dataset);
+
+                        // make display popup only if not visible already and hide if displaed already
+                        if (display && !$selector.getVisibility()) {
+                            $selector.setPosition(e.cursorPosition);
+                            $selector.setVisible(true);
+                        }
+                        else if (!display && $selector.getVisibility()) {
+                            $selector.setVisible(false);
+                        }
+                    }
+                },
+                onKeydown: function (e) {
+
+                    var keyCode = e.keyCode;
+                    var $selector = _OptionSelector;
+
+                    // filter keys only if the popup is displayed.
+                    if ($selector.getVisibility()) {
+                        if (keyCode === 27) {
+                            //escape - this will cancel the option selector.
+                            e.preventDefault();
+                            $selector.setVisible(false);
+                        }
+                        else if (keyCode === 38) {
+                            //up - this will highlight previous option in the list.
+                            e.preventDefault();
+                            $selector.setSelected($selector.getSelectedIndex() - 1);
+                        }
+                        else if (keyCode === 40) {
+                            //down - this will highlight the next option in the list.
+                            e.preventDefault();
+                            $selector.setSelected($selector.getSelectedIndex() + 1);
+                        }
+                        else if (keyCode === 13 || (keyCode === 32 && _Settings.selectOnSpacebar)) {
+                            //enter - this will process the current state of entry attached to the tag property of option selector
+                            e.preventDefault();
+                            $selector.setVisible(false);
+                            processSelection($selector, $element);
+                        }
+                        else if (keyCode === 9) {
+                            //tab - this will move the focus off the control so hide popup.
+                            $selector.setVisible(false);
+                        }
+                    }
+                }
+            }
+        });
+
+        var processSelection = function (optionSelector, $box) {
+
+            var box = $box.get(0);
+            var tag = optionSelector.tag;
+            var trigger = tag.data[0];
+            var replacement = trigger.replace(optionSelector.getSelected());
+            var newpreText = null;
+            var newpostText = null;
+
+            if (_Settings.fullTextMatching) {
+                if ($.isArray(replacement)) {
+                    newpreText = tag.preText.replace(trigger.match, replacement[0]);
+                    newpostText = replacement[1] + tag.postText;
+                }
+                else {
+                    newpreText = tag.preText.replace(trigger.match, replacement);
+                    newpostText = tag.postText;
+                }
+            }
+            else {
+                if ($.isArray(replacement)) {
+                    newpreText = tag.preText.substring(0, _CachedLocation) + replacement[0];
+                    newpostText = replacement[1] + tag.postText;
+                }
+                else {
+                    newpreText = tag.preText.substring(0, _CachedLocation) + replacement;
+                    newpostText = tag.postText;
+                }
+            }
+
+            $box.val(newpreText + newpostText);
+
+            if (typeof box.selectionEnd === "number") {
+                box.selectionStart = box.selectionEnd = newpreText.length;
+            }
+            else if (box.createTextRange) {
+                var range = box.createTextRange();
+                range.move('character', newpreText.length);
+                range.collapse(false);
+                range.select();
+            }
+
+        }
+
+        // attached option selector to input manager.
+        _InputManager.getUIRoot().append(_OptionSelector.getUIRoot());
+
+        // register event to hide popup when clicked anywhere else on the page.
+        $(document.body).bind('click', function (e) {
+            if ($(e.target).closest(_OptionSelector).length == 0) {
+                _OptionSelector.setVisible(false);
+            }
+        });
+
+    }
+
+    $.fn.inputWizard = function (options) {
+
+        return this.each(function () {
+            if (undefined == $(this).data('inputWizard')) {
+                var plugin = new $.inputWizard(this, options);
+                $(this).data("inputWizard", plugin);
+            }
+        });
+
+        return this;
+    }
+
+
+    // global cache object.
+    var searchCache = {};
+
+    $.fn.inputWizard.setSearchCache = function (term, results) {
+        searchCache[term] = results;
+    }
+
+    $.fn.inputWizard.getSearchCache = function (term) {
+        return searchCache[term];
+    }
+
+
+
+})(window.jQuery);
---
+++ Company.Web.JQuery.InputWizard/Scripts/optionSelector.js
@@ -0,0 +1,162 @@
+﻿// This code is provided in raw form.
+// It should be modified according to javascript specification at any time.
+; function optionSelector(options) {
+
+    "use strict";
+
+    //Settings
+    var _Settings = $.extend({}, {
+        defaultIndex: 0,
+        listClass: "selectorlist",
+        itemClass: "selectorlist-item",
+        selectedItemClass: "selectorlist-selecteditem",
+        rootTemplate: "<ul style='padding: 0; margin: 0; list-style-type: none; z-index: 999999;'></ul>",
+        itemTemplate: "<li></li>",
+        itemdataattribute: "index",
+        events: {
+            onClicked: null,
+            OnSelectionChanged: null
+        }
+    }, options);
+
+    this.tag = null;
+
+    //Properties 
+    var _Data = [];
+    var _SelectedIndex = -1;
+    var _IsVisible = true;
+    var _UIRoot = $(_Settings.rootTemplate).addClass(_Settings.listClass);
+    var _DisplayType = _UIRoot.css("display");
+
+    this.getSettings = function () {
+        return _Settings;
+    }
+
+    this.getUIRoot = function () {
+        return _UIRoot;
+    }
+
+    this.getData = function () {
+        return _Data;
+    }
+
+    this.getSelectedIndex = function () {
+        return _SelectedIndex;
+    }
+
+    this.getDisplayType = function () {
+        return _DisplayType;
+    }
+
+    this.getVisibility = function () {
+        return _IsVisible;
+    }
+
+    //Public - Clears the list items.
+    this.clear = function () {
+        _Data = [];
+        _UIRoot.empty();
+        _SelectedIndex = -1;
+    }
+
+    //Public - Creates list items by removing previous.
+    this.setData = function (data) {
+        this.clear();
+        if (data && data.length > 0) {
+            _Data = data;
+            initialRender(this);
+            this.setSelected(_Settings.defaultIndex);
+        }
+    }
+
+    //public - return selected data item.
+    this.getSelected = function () {
+        if (_SelectedIndex > -1) {
+            return _Data[_SelectedIndex];
+        }
+        return null;
+    }
+
+    //Public - sets selected list item.
+    this.setSelected = function (index) {
+        var $children = _UIRoot.children();
+        var $settings = _Settings;
+
+        if (index > -1 && index < $children.length) {
+            $children.each(function (i, item) {
+                decorateItem($settings.selectedItemClass, $(item), index, i);
+            });
+            _SelectedIndex = index;
+            if ($settings.events.OnSelectionChanged != null) {
+                $settings.events.OnSelectionChanged.call(this);
+            }
+        }
+    }
+
+    //Public - changes visibility of the list.
+    this.setVisible = function (visible) {
+        if (visible && !_IsVisible) {
+            _UIRoot.css("display", _DisplayType);
+            _IsVisible = true;
+        }
+        else if (!visible && _IsVisible) {
+            _DisplayType = _UIRoot.css("display");
+            _UIRoot.css("display", "none");
+            _IsVisible = false;
+        }
+    }
+
+    //Public - changes locatin of the list within the page.
+    this.setPosition = function (location) {
+        _UIRoot.css(location);
+    }
+
+    //Private Event - raised when a list item is clicked
+    var onClicked = function (e) {
+        var $listitem = $(e.target);
+        var $settings = _Settings;
+        this.setSelected(parseInt($listitem.data($settings.itemdataattribute), 10));
+        if ($settings.events.onClicked != null) {
+            $settings.events.onClicked.call(this, e);
+        }
+    }
+
+    //Private Static - creates list items using data property
+    // To use complete data item structure such as { name: "anyname", data: "anyhash" } change this method.
+    // To use elements other than list change this method.
+    var initialRender = function ($this) {
+        var $settings = $this.getSettings();
+        var $root = $this.getUIRoot();
+        $root.empty();
+        $.each($this.getData(), function (i, item) {
+            var listItem = $($settings.itemTemplate).attr('data-' + $settings.itemdataattribute, i).addClass($settings.itemClass).append(item);
+            decorateItem($settings.selectedItemClass, listItem, $this.getSelectedIndex(), i);
+            $root.append(listItem);
+        });
+    }
+
+    //Private Static - decorates item by adding/removing css classes
+    var decorateItem = function (cssClass, item, selIndex, currIndex) {
+        if (selIndex != currIndex) {
+            item.removeClass(cssClass);
+        }
+        else if (!item.hasClass(cssClass)) {
+            item.addClass(cssClass);
+        }
+    }
+
+    //Private Static - changes function scope so that 'this' could point to source object
+    var contextBinder = function (func, scope) {
+        if (func.bind) {
+            return func.bind(scope);
+        } else {
+            return function () {
+                func.apply(scope, arguments);
+            };
+        }
+    };
+
+    // binds all list items to onClicked event in one go.
+    // If we change the ui element from List to something else we need to handle the event binding below.
+    _UIRoot.on('click', 'li', contextBinder(onClicked, this));
+}
