b2757206377661a5337c134a4c8eb083451c60d9
SnippetsUnlimited <ayyaz_ahmed@hotmail.com>
2014-02-11 20:46:01 -0500

    update

---
+++ Company.Web.JQuery.JQueryBoilerPlate/Scripts/customPlugin.js
@@ -0,0 +1,36 @@
+﻿;(function ($) {
+
+    "use strict";
+
+    $.customPlugin = function (element, options) {
+
+        var $element = $(element);
+
+        var $settings = $.extend({}, {
+            key: null
+        }, options);
+
+
+        this.customFunction1 = function () {
+            alert($settings.key);
+        }
+
+        this.customFunction2 = function () {
+            alert($settings.key);
+        }
+
+    }
+
+    $.fn.customPlugin = function (options) {
+
+        return this.each(function () {
+            if (typeof $(this).data("customPlugin") === "undefined") {
+                var plugin = new $.customPlugin(this, options);
+                $(this).data("customPlugin", plugin);
+            }
+        });
+
+        return this;
+    }
+
+})(window.jQuery);
